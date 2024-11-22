5f1993463a090a6fc213ad6e572d4bc09c0792cf
nowsheen1 <n7sharmili@gmail.com>
2024-11-22 17:48:06 -0600

    Add files via upload

---
+++ geometry_co/Sub_system_2_opt.m
@@ -0,0 +1,29 @@
+function X_Sub_sys_2=Sub_system_2_opt(x1s,x2s)
+global count;
+FUN = @Sub_sys_2_obj;
+X0 = [35;-65]; % x(1)=xs,x(2)=t1,x(3)=t2
+A = [];
+B = [];
+Aeq = [];
+Beq = [];
+LB = [0;-100];
+UB = [100;100];
+NONLCON = @constraints_Sub_sys_2;
+%options = optimset('PlotFcns','optimplotfval','TolX',1e-7,'MaxIter',100000,'MaxFunEvals',100000,'Algorithm','sqp');
+%options=optimoptions('fmincon','Algorithm','interior-point','MaxFunEvals' ,100000,'MaxIter' ,100000,'TolX',1e-100,'TolFun',1e-10,'Display','iter');
+options=optimoptions('fmincon','Algorithm','interior-point','MaxFunEvals' ,1000,'MaxIter' ,1000,'TolX',1e-100,'TolFun',1e-10,'Display','iter');
+%options = optimoptions(options,'MaxFunEvals' ,100000);
+%%TolFun is optimality tolerance%%fmincon takes the last option so be
+%%careful about the options.
+%options = optimoptions(options,'MaxIter' ,100000);
+%options = optimset ('LargeScale', 'off', 'TolCon', 1e-8, 'TolX', 1e-8, 'TolFun',1e-7);
+[X_Sub_sys_2,fval2,exitflag2,Output2] = fmincon(FUN,X0,A,B,Aeq,Beq,LB,UB,NONLCON,options);
+function f= Sub_sys_2_obj(x)
+f=((x(1)-x1s)^2+(x(2)-x2s)^2);
+end
+function [c,ceq]=constraints_Sub_sys_2(x)
+c(1)=0.1*((1.5*x(1)-20)^2)-70-x(2);
+c(2)=(50/(x(1)+0.1))-40-x(2);
+ceq=[];
+end
+end
---
+++ geometry_co/System_optimization.m
@@ -0,0 +1,29 @@
+FUN = @Sys_obj;
+global count count1 count_sys;
+count_sys=0;
+count=0;
+count1=0;
+X0 = [35;-65]; % x(1)=xs,x(2)=t1,x(3)=t2
+A = [];
+B = [];
+Aeq = [];
+Beq = [];
+LB = [0;-100];
+UB = [100;100];
+NONLCON = @constraints_Sys;
+%options = optimset('PlotFcns','optimplotfval','TolX',1e-7,'MaxIter',100000,'MaxFunEvals',100000,'Algorithm','sqp');
+%options=optimoptions('fmincon','Algorithm','interior-point','MaxFunEvals' ,100000,'MaxIter' ,100000,'TolX',1e-100,'TolFun',1e-10,'Display','iter');
+
+options=optimoptions('fmincon','MaxFunEvals' ,300,'MaxIter' ,1000,'TolX',1e-6,'TolFun',1e-6,'TolCon',10^-2,'Display','iter','FiniteDifferenceStepSize',10^-8,'ScaleProblem', true);
+%
+%options=optimoptions('fmincon','MaxFunEvals' ,1000000,'MaxIter' ,10000000,'TolX',1e-16,'TolFun',1e-16,'TolCon',10^-16,'Display','iter','FiniteDifferenceStepSize',10^-8,'ScaleProblem', true);
+%options=optimoptions('fmincon','ScaleProblem', true);
+%options = optimoptions(options,'MaxFunEvals' ,100000);
+%%TolFun is optimality tolerance%%fmincon takes the last option so be
+%%careful about the options.
+%options = optimoptions(options,'MaxIter' ,100000);
+%options = optimset ('LargeScale', 'off', 'TolCon', 1e-8, 'TolX', 1e-8, 'TolFun',1e-7);
+[X,fval,exitflag,Output] = fmincon(FUN,X0,A,B,Aeq,Beq,LB,UB,NONLCON,options);
+z=count;
+h=count1;
+cn=count_sys
